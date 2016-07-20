3cbebfbd0e396a124a7ea8aefadc36b7b1ab2b7e
Kyle Winckler <kyle.winckler@gmail.com>
2016-07-19 20:16:37 -0700

    added js filed to index.html

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -3,7 +3,12 @@ var articles = [];
 function Article (opts) {
   // TODO: Use the js object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorURL = opts.authorURL;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
@@ -22,7 +27,7 @@ Article.prototype.toHtml = function() {
   // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
 
-  $newArticle.append('<hr>');
+  $newArticle.append('<hr>')
 
   // TODO: This cloned article is no longer a template, so we should remove that class...
 
@@ -37,6 +42,8 @@ rawData.forEach(function(ele) {
   articles.push(new Article(ele));
 })
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
-});
+// articles.forEach(function(a){
+//   $('#articles').append(a.toHtml())
+// });
+var $h1 = $('article h1').text();
+console.log($h1);
