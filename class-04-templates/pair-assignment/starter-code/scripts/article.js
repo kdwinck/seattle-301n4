f5e6ff36a0f9307565a3644c0b57bde30299f405
Kyle Winckler <kyle.winckler@gmail.com>
2016-07-26 20:31:02 -0700

    incorperated handlebars to create a template for blog articles

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -13,15 +13,27 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+  var source = $('#blog-template').html();
+  var template = Handlebars.compile(source);
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
+
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  var data = {Title: this.title,
+              Author: this.author,
+              Published: this.publishStatus,
+              Body: this.body,
+              cat: this.category
+            };
+  var html = template(data);
+
+  return html;
 };
 
 rawData.sort(function(a,b) {
@@ -30,8 +42,9 @@ rawData.sort(function(a,b) {
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
 
 articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+  $('#articles').append(a.toHtml());
+  $('#articles').append('<hr>');
 });
