74bb0735d3b59020e6e9e18f2fd4fe82b738d9d2
Kyle Winckler <kyle.winckler@gmail.com>
2016-08-09 20:39:56 -0700

    did the shit

--- class-08-crud-a-resource/pair-assignment/starter-code/scripts/article.js
+++ class-08-crud-a-resource/pair-assignment/starter-code/scripts/article.js
@@ -22,7 +22,13 @@
   // TODO: Set up a DB table for articles.
   Article.createTable = function(callback) {
     webDB.execute(
-      '...', // what SQL command do we run here inside these quotes?
+      'CREATE TABLE IF NOT EXISTS articles(' + // what SQL command do we run here inside these quotes?
+        'title VARCHAR(255) NOT NULL,' +
+        'category VARCHAR(255) NOT NULL,' +
+        'author VARCHAR(255) NOT NULL,' +
+        'authorUrl TEXT NOT NULL,' +
+        'publishedOn DATE,' +
+        'body TEXT);',
       function(result) {
         console.log('Successfully set up the articles table.', result);
         if (callback) callback();
@@ -33,7 +39,7 @@
   // TODO: Use correct SQL syntax to delete all records from the articles table.
   Article.truncateTable = function(callback) {
     webDB.execute(
-      'DELETE ...;', // <----finish the command here, inside the quotes.
+      'DELETE * FROM articles;', // <----finish the command here, inside the quotes.
       callback
     );
   };
@@ -44,8 +50,8 @@
     webDB.execute(
       [
         {
-          'sql': '...;',
-          'data': [],
+          'sql': 'INSERT INTO articles (title, category, author, authorUrl, publishedOn, body) VALUES (?, ?, ?, ?, ?, ?);',
+          'data': [this.title, this.category, this.author, this.authorUrl, this.publishedOn, this.body],
         }
       ],
       callback
@@ -57,7 +63,8 @@
     webDB.execute(
       [
         {
-          /* ... */
+          'sql': 'DELETE * FROM articles WHERE id = ?;',
+          'data': [id]
         }
       ],
       callback
@@ -69,7 +76,8 @@
     webDB.execute(
       [
         {
-          /* ... */
+          'sql': 'UPDATE FROM articles SET (title, category, author, authorUrl, publishedOn, body) VALUES (?, ?, ?, ?, ?, ?) WHERE id = ?;',
+          'data': [id, this.title, this.category, this.author, this.authorUrl, this.publishedOn, this.body, id]
         }
       ],
       callback
@@ -86,25 +94,27 @@
   // TODO: Refactor this to check if the database holds any records or not. If the DB is empty,
   // we need to retrieve the JSON and process it.
   // If the DB has data already, we'll load up the data (sorted!), and then hand off control to the View.
-  Article.fetchAll = function() {
-    webDB.execute('', function(rows) { // TODO: fill these quotes to 'select' our table.
+  Article.fetchAll = function(callback) {
+    webDB.execute('SELECT * FROM articles', function(rows) { // TODO: fill these quotes to 'select' our table.
       if (rows.length) {
         // TODO: Now, 1st - instanitate those rows with the .loadAll function,
+        Article.loadAll(rows);
         // and 2nd - pass control to the view by calling whichever function argument was passed in to fetchAll.
-
+        callback();
       } else {
         $.getJSON('/data/hackerIpsum.json', function(rawData) {
           // Cache the json, so we don't need to request it next time:
           rawData.forEach(function(item) {
             var article = new Article(item); // Instantiate an article based on item from JSON
             // TODO: Cache the newly-instantiated article in the DB: (what can we call on each 'article'?)
-
+            article.insertRecord();
           });
           // Now get ALL the records out the DB, with their database IDs:
-          webDB.execute('', function(rows) { // TODO: select our now full table
+          webDB.execute('SELECT * From articles', function(rows) { // TODO: select our now full table
             // TODO: Now, 1st - instanitate those rows with the .loadAll function,
             // and 2nd - pass control to the view by calling whichever function argument was passed in to fetchAll.
-
+            Article.loadAll(rows);
+            callback();
           });
         });
       }
@@ -140,13 +150,13 @@
           return a.author === author;
         })
         .map(function(a) {
-          return a.body.match(/\b\w+/g).length
+          return a.body.match(/\b\w+/g).length;
         })
         .reduce(function(a, b) {
           return a + b;
         })
-      }
-    })
+      };
+    });
   };
 
   Article.stats = function() {
@@ -155,7 +165,7 @@
       numWords: Article.numwords(),
       Authors: Article.allAuthors(),
     };
-  }
+  };
 
   module.Article = Article;
 })(window);
--- class-08-crud-a-resource/pair-assignment/starter-code/scripts/articleView.js
+++ class-08-crud-a-resource/pair-assignment/starter-code/scripts/articleView.js
@@ -107,7 +107,7 @@
 
   articleView.initIndexPage = function() {
     Article.all.forEach(function(a){
-      $('#articles').append(a.toHtml())
+      $('#articles').append(a.toHtml());
     });
 
     articleView.populateFilters();
@@ -123,7 +123,7 @@
 
     Article.numWordsByAuthor().forEach(function(stat) {
       $('.author-stats').append(template(stat));
-    })
+    });
 
     $('#blog-stats .articles').text(Article.all.length);
     $('#blog-stats .words').text(Article.numWordsAll());
